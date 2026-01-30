952b9ec8a18e0517f004170a9967e79fe82200fc
ArcNinjaC <121591277+ArcNinjaC@users.noreply.github.com>
2026-01-30 17:40:16 -0500

    basic program to take pics

--- src/main.py
+++ src/main.py
@@ -1,18 +1,32 @@
 from picamzero import Camera
+import time
+
+SAMPLE_RATE = 1 # per second
+TIME_STEP = 1/SAMPLE_RATE
 
 cam = Camera()
 
-cam.take_photo("image1.jpg")
+def test():
+    picture_index = 0
+    frame_time = 0
+    start = time.monotonic()
+    last_frame_end = time.monotonic()
+    while time.monotonic() - start < 10:
+        cam.take_photo(f"picture{picture_index}_time:{time.monotonic()-start}")
+        picture_index += 1
+        frame_time = time.monotonic()- last_frame_end
+        if frame_time < TIME_STEP:
+            # time.sleep(TIME_STEP - frame_time)
+            pass # for now imma just take pics as quickly as possible
+        last_frame_end = time.monotonic()
+    
+    return 100 # random exmample
 
-
-def accelerometerCalculation():
-    return 1000
 
 
 with open("result.txt", "w") as f:
-    speed = accelerometerCalculation()
-    speed = str(round(speed,5)) 
-    f.write(speed)
+    speed = test()
+    f.write(str(round(speed,5)))
 
 
 
